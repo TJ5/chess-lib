7118e4b32ede4c681d17283a36ed17c8a0a3d569
TJ5 <tejesh.anand@hotmail.com>
2022-01-05 17:26:14 -0800

    main

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "BitBoard.h"
+#include <bitset>
+using namespace std;
+
+int main() {
+    //Tests only
+
+
+    /**
+    BitBoard s;
+    int len;
+    BitBoard* y = s.getLegalBoards(0, &len);
+    
+    delete[] y;
+    */
+    
+    return 0;
+}
